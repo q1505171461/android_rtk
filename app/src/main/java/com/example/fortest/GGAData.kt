f838b484f2a2ecc12640449e30c63475e36311e5
hu <hu>
2024-02-23 16:05:49 +0800

    1

--- app/src/main/java/com/example/fortest/GGAData.kt
+++ app/src/main/java/com/example/fortest/GGAData.kt
@@ -1,4 +1,14 @@
 package com.example.fortest
 
+import com.github.mikephil.charting.data.Entry
+import kotlin.collections.HashMap
+
 object GGAData {
+    val data : HashMap<Long,ArrayList<Double>> = HashMap()
+    fun  addGga(gga: String){
+
+    }
+//    fun  gga2blh():ArrayList<Double>{
+//
+//    }
 }
